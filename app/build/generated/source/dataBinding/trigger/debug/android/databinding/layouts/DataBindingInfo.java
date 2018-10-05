3cf1a553bb277e72a35d7758670d863fea06c694
Shahbaz Khan <shahbaz.khan@bookmyshow.com>
2018-10-05 12:29:39 +0530

    Deleted build folder

--- app/build/generated/source/dataBinding/trigger/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="77f9bfca-6949-4de0-80a8-b34e43268597")
-public class DataBindingInfo {}
